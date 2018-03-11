d2e8f674ef652fb57f7bac907bb32b26f9b3a9c6
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-03-11 15:45:48 +0300

    Update BSTree.cpp

--- sources/BSTree.cpp
+++ sources/BSTree.cpp
@@ -4,39 +4,43 @@
 using namespace BSTree;
 using namespace std;
 
-Tree::Tree() { head = nullptr; }
-auto Tree::fill_tree(int mass) -> void {
-  Node *curr;
-  curr = head;
-  Node *node = new Node{mass, nullptr, nullptr, nullptr};
-  if (head == nullptr) {
-    head = node;
-    return;
-  }
-  while (true) {
-    if ((curr->data) < (node->data) && curr->right != nullptr) {
-      curr = curr->right;
-    } else if ((curr->data) < (node->data) && curr->right == nullptr) {
-      curr->right = node;
-      node->prev = curr;
-      break;
-    }
+Tree::Tree() {
+	root = nullptr;
+}
+auto Tree::insert(int mass) -> void {
+	Node *curr;
+	curr = root;
+	Node *node = new Node{mass, nullptr, nullptr, nullptr};
+	if (root == nullptr) {
+		root = node;
+		return;
+	}
+	while (true) {
+		if ((curr->data) < (node->data) && curr->right != nullptr) {
+			curr = curr->right;
+		} else if ((curr->data) < (node->data) && curr->right == nullptr) {
+			curr->right = node;
+			node->parent = curr;
+			break;
+		}
 
-    if ((curr->data) > (node->data) && curr->left != nullptr) {
-      curr = curr->left;
-    } else if ((curr->data) > (node->data) && curr->left == nullptr) {
-      curr->left = node;
-      node->prev = curr;
-      break;
-    }
-  }
+		if ((curr->data) > (node->data) && curr->left != nullptr) {
+			curr = curr->left;
+		} else if ((curr->data) > (node->data) && curr->left == nullptr) {
+			curr->left = node;
+			node->parent = curr;
+			break;
+		}
+	}
 }
 auto Tree::delete_tree(Node *&node) -> void {
-  if (node != nullptr) {
-    delete_tree(node->left);
-    delete_tree(node->right);
-    delete node;
-    node = nullptr;
-  }
+	if(node!=nullptr) {
+		delete_tree(node->left);
+		delete_tree(node->right);
+		delete node;
+		node=nullptr;
+	}
 }
-Tree::~Tree() { delete_tree(head); }
+Tree::~Tree() {
+	delete_tree(root);
+}
