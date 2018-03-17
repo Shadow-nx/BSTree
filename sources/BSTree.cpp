30efb6928031351868347201cdb895e040292feb
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-03-17 12:56:34 +0300

    Update BSTree.cpp

--- sources/BSTree.cpp
+++ sources/BSTree.cpp
@@ -7,45 +7,49 @@ using namespace std;
 Tree::Tree() {
 	root = nullptr;
 }
-Node* Tree::getRoot(){
+Node* Tree::getRoot() {
 	return root;
 }
 auto Tree::show_tree(Node* root,int stage)->void {
 	if(root->left != nullptr)
-		show_tree(root->left, stage + 1);
-	if(root != this->root) {
+		show_tree(root->left,stage+1);
+	if(root!=this->root) {
 		cout.width(stage*4);
-		cout << "--";
+		cout<<"--";
 	}
-	cout << "(" << root->data << ")" << endl;
-	if(root->right != nullptr)
+	cout<<"("<< root->data <<")"<< endl;
+	if(root->right!=nullptr)
 		show_tree(root->right,stage+1);
 }
-auto Tree::insert(int mass) -> void {
-	Node *curr;
-	curr = root;
-	Node *node = new Node{mass, nullptr, nullptr, nullptr};
+auto Tree::insert(int value) -> bool {
 	if (root == nullptr) {
-		root = node;
-		return;
+		root = new Node{value, nullptr, nullptr};
 	}
-	while (true) {
-		if ((curr->data) < (node->data) && curr->right != nullptr) {
-			curr = curr->right;
-		} else if ((curr->data) < (node->data) && curr->right == nullptr) {
-			curr->right = node;
-			node->parent = curr;
-			break;
-		}
 
-		if ((curr->data) > (node->data) && curr->left != nullptr) {
-			curr = curr->left;
-		} else if ((curr->data) > (node->data) && curr->left == nullptr) {
-			curr->left = node;
-			node->parent = curr;
-			break;
+	Node* current = root;
+	Node* previous = root;
+	while (current != nullptr) {
+		if (current->data > value) {
+			current = current->left;
+		}
+		else if (current->data < value){
+			current = current->right;
+		}
+		else {
+			return false;
+		}
+		if (current != nullptr) {
+			previous = current;	
 		}
 	}
+
+	if (previous->data > value) {
+		previous->left = new Node{value, nullptr, nullptr};
+	}
+	else {
+		previous->right = new Node{value, nullptr, nullptr};
+	}
+	return true;
 }
 auto Tree::delete_tree(Node *&node) -> void {
 	if(node!=nullptr) {
