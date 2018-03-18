e4d94a4a4ab25aa62e50cadb239f9ede79e459eb
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-03-18 14:47:19 +0300

    Update BSTree.hpp

--- include/BSTree.hpp
+++ include/BSTree.hpp
@@ -11,10 +11,10 @@ namespace BSTree {
 			auto delete_tree(Node *&node) ->void;
 		public:
 			Tree();
-			auto getRoot()->Node*&;
-		        auto empty()->bool;
+			auto empty()->void;
+			auto add_to_tree(int value)->bool;
 			auto show_tree(Node* root,int stage)->void;
-			auto insert(Node*& root,int value)-> bool;
+			auto insert(Node*& root,Node*& parent,int value)-> Node*;
 			~Tree();
 	};
 }
