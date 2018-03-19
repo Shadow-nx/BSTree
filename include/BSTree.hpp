24b1f1496da1ad68eff99169ccddd11faeb0f066
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-03-19 22:31:47 +0300

    Update BSTree.hpp

--- include/BSTree.hpp
+++ include/BSTree.hpp
@@ -11,10 +11,10 @@ namespace BSTree {
 			auto delete_tree(Node *&node) ->void;
 		public:
 			Tree();
-		   	auto empty()->bool;
-			auto show_tree(Node* root,int stage)->void;
-			auto insert(Node*& root,Node*& parent,int value)->Node*;
-			auto add_to_tree(int value)->bool;
+		   	auto empty() -> bool;
+			auto show_tree(Node* root,int stage) -> void;
+			auto insert(Node*& root,Node*& parent,int value) -> Node*;
+			auto add_to_tree(int value) -> bool;
 			~Tree();
 	};
 }
