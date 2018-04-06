6ed7812b321eb01da00ba9a1b77939c9b4c9a9f0
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-04-06 13:26:04 +0300

    Update BSTree.hpp

--- include/BSTree.hpp
+++ include/BSTree.hpp
@@ -9,11 +9,13 @@ namespace BSTree {
 		private:
 			Node* root;
 			auto insert(Node*& root,Node*& parent,int value) -> Node*;
+			auto insert(Node*& root,Node*& parent,Node*& value) -> Node*;
 			auto show(Node* root,int stage) const -> void;
 			auto delete_tree(Node *&node) -> void;
 			auto direct_bypass(Node* root) const -> void;
 			auto symmetric_bypass(Node* root) const -> void;
 			auto back_bypass(Node* root) const -> void;
+			auto delete_node(Node *&root,int value) -> void;
 		public:
 			enum order{direct, symmetric, back};
 			Tree();
@@ -21,6 +23,7 @@ namespace BSTree {
 			auto show() const -> void;
 			auto insert(int value) -> bool;
 			auto version_bypass(order) const -> void;
+			auto delete_node(int value) -> void;
 			~Tree();
 	};
 }
