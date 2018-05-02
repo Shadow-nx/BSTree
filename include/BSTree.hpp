d9e5b5c7d914eabe9678e0ec33be7049aec0d861
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-05-02 15:25:39 +0300

    Update BSTree.hpp

--- include/BSTree.hpp
+++ include/BSTree.hpp
@@ -1,9 +1,9 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 namespace BSTree {
-	#include <fstream>
-	#include <iostream>
-	
-	using namespace std;
-	
+	enum order {direct, symmetric, back};
 	struct Node {
 		int data;
 		Node *left;
@@ -16,22 +16,27 @@ namespace BSTree {
 			auto insert(Node*& root,Node*& parent,int value) -> Node*;
 			auto insert(Node*& root,Node*& parent,Node*& value) -> Node*;
 			auto show(Node* root,int stage) const -> void;
+			auto show(Node* root,std::ostream& os,int stage) const -> void;
 			auto delete_tree(Node *&node) -> void;
-			auto direct_bypass(Node* root) const -> void;
-			auto symmetric_bypass(Node* root) const -> void;
-			auto back_bypass(Node* root) const -> void;
-			auto delete_node(Node *&root,int value) -> void;
+			auto direct_bypass(Node* root,std::vector<int>& numbers_array) const -> void;
+			auto direct_bypass(Node* root, std::ofstream &fout) const -> void;
+			auto symmetric_bypass(Node* root,std::vector<int>& numbers_array) const -> void;
+			auto back_bypass(Node* root,std::vector<int>& numbers_array) const -> void;
+			auto delete_node(Node *&root,int value) -> bool;
 		public:
-			enum order{direct, symmetric, back};
 			Tree();
-		   	auto empty() const -> bool;
-			auto show() const -> void;
+			Tree(std::initializer_list<int> list);
+			Tree(const Tree& tree);
+			auto exists(int value) ->bool;
 			auto insert(int value) -> bool;
+			auto delete_node(int value) -> bool;
+			auto save_to_file(const std::string& path) -> bool;
+			auto load_from_file(const std::string& path)-> bool;
 			auto version_bypass(order) const -> void;
-			auto direct_bypass(Node* root, ofstream &fout) const -> void;
-	                auto save_to_file() -> void;
-		        auto load_from_file()-> void;
-			auto delete_node(int value) -> void;
+			auto friend operator<<(std::ostream& os,const Tree& tr) -> std::ostream&;
+			auto operator=(const Tree&) -> Tree&;
 			~Tree();
+			//auto show() const -> void;
+			//auto empty() const -> bool;
 	};
 }
